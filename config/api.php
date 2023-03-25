a206a146d4578619a327536c18916e7628160c66
Esi Ogagaoghene <30408871+ogagaoghene@users.noreply.github.com>
2023-03-25 20:24:07 +0100

    Delete api.php

--- config/api.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace app\config;
-
-define('PROJECT_DOCUMENT_ROOT', dirname(__DIR__));
-define('URLROOT', ($_SERVER['HTTP_HOST'] == 'localhost')?'http://localhost/hms':$_SERVER['HTTP_HOST']);
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASSWORD', '');
